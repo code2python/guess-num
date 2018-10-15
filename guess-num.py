57b79fc3bf1d24cd625df914155e6a6b3c979b72
Jerry Tseng <0817jerry@gmail.com>
2018-10-15 11:27:06 +1100

    updated

--- guess-num.py
+++ guess-num.py
@@ -1,14 +1,19 @@
 import random 
 
 r = random.randint (1, 100)
+count = 0
 while True:
 	num = input('Please guess a number: ')
 	num = int (num)
+	count += 1  # count = count + 1  
 	if num == r:
 		print('Bingo!!')
+		print ('this is your', count, 'guess')
 		break
 	elif num > r:
 		print ('the number that you have enter is bigger than me')
 
 	elif num < r:
 		print ('the number that you have enter is smaller than me')
+
+	print ('this is your', count, 'guess')
